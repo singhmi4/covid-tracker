fcec5c079b73a6d41731147f1af36814213f8c81
Michael Singh <singh.michaelanthony@gmail.com>
2020-07-17 19:28:12 -0400

    fetch data from api

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ import { Cards, Chart, CountryPicker } from './components';
 // App Styling
 import styles from './App.module.css';
 
+// API
+import { fetchData } from './api';
+
 class App extends React.Component {
+
+    async componentDidMount() {
+        const data = await fetchData();
+        console.log(data);
+    }
+
     render() {
 
         return (
