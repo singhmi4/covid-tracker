5f5e2a90f2f55a044ece719c0ff7cc559ee31102
Michael Singh <singh.michaelanthony@gmail.com>
2020-07-17 20:03:24 -0400

    display fetchData onto card components

--- src/App.js
+++ src/App.js
@@ -10,17 +10,22 @@ import styles from './App.module.css';
 import { fetchData } from './api';
 
 class App extends React.Component {
+    state = {
+        data: {},
+    }
 
     async componentDidMount() {
-        const data = await fetchData();
-        console.log(data);
+        const fetchedData = await fetchData();
+
+        this.setState({ data: fetchedData })
     }
 
     render() {
-
+        // destructure the data object from the state
+        const { data } = this.state;
         return (
             <div className={styles.container}>
-                <Cards />
+                <Cards data={data} /> 
                 <CountryPicker />
                 <Chart />
             </div>
