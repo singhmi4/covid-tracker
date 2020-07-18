533201c6b2ec2beb28f9bb579d13d55b334f4c56
Michael Singh <singh.michaelanthony@gmail.com>
2020-07-17 23:06:20 -0400

    refactor card grid items

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -33,20 +33,6 @@ const Cards = ( {data: { confirmed, recovered, deaths, lastUpdate } }) => {
         },
     ]
 
-
-  
-
-    // {cardData}.map((data) => ({
-    //     <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.data.type)}>
-    //         <Typography color="textSecondary" gutterBottom>data.title</Typography>
-    //         <Typography variant="h5">
-    //             <CountUp start={0} end={data.type} duration={2.5} separator="," />
-    //         </Typography>
-    //         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-    //         <Typography variant="body2">data.body</Typography>
-    //     </Grid>
-    // }));
-
     return(
         <div className={styles.container} >
              <Grid container spacing={3} justify="center" >
